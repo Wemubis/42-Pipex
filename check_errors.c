b7794eb2f4dbdc02c118c3ea25823e1a96b0ab17
Ghost <108669382+Wemubis@users.noreply.github.com>
2022-12-15 09:41:38 +0100

    create files

---
+++ check_errors.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_errors.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mle-boud <mle-boud@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/14 17:35:07 by mle-boud          #+#    #+#             */
+/*   Updated: 2022/12/14 17:35:07 by mle-boud         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+// check number of argc == 5
+// check if 1st is file, then 2nd and 3rd cmd and 4th file (nb 0 is ./pipex)
+//			check files exists
+//			check cmds exists
+// if errors, return 2, otherwise return 0
+
+int	check_errors(int ac, char **av)
+{
+}
