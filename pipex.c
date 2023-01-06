32ac60aeb35fe797945cf0db08710878ad77760d
Ghost <108669382+Wemubis@users.noreply.github.com>
2023-01-06 10:06:29 +0100

    Add files via upload

--- pipex.c
+++ pipex.c
@@ -12,6 +12,58 @@
 
 #include "pipex.h"
 
-int	main(int ac, char **av)
+int ft_pipex(char **av)
 {
+    int     fd[2];
+    char    **cmd[2];
+    int     child_pid[2];
+
+    cmd[0] = ft_split(av[2], " ");
+    cmd[1] = ft_split(av[3], " ");
+    check_errors(ac, av, cmd[0][0], cmd[1][0]);
+    fd[0] = av[1];
+    fd[1] = av[4];
+    if (pipe(fd) == -1)
+    {
+        perror("pipe");
+        return (2);
+    }
+    
+    child_pid[0] = fork();
+    if (child_pid[0] < 0)
+    {
+        perror("fork child_pid[0]");
+        return (2);
+    }
+    if (child_pid[0] == 0)
+    {
+        // before the pipe
+        dup2(fd[1], STDOUT_FILENO);
+        close(fd[0]);
+        close(fd[1]);
+        execve(cmd[0][0], cmd[0], NULL);
+    }
+
+    child_pid[1] = fork();
+    if (child_pid[1] < 0)
+    {
+        perror("fork child_pid[1]");
+        return (2);
+    }
+    if (child_pid[1] == 0)
+    {
+        // after the pipe
+        dup2(fd[0], STDIN_FILENO);
+        close(fd[0]);
+        close(fd[1]);
+        execve(cmd[1][0], cmd[1], NULL);
+    }
+
+    close(fd[0]);
+    close(fd[1]);
+
+    waitpid(child_pid[0], NULL, 0);
+    waitpid(child_pid[1], NULL, 0);
+
+    return (0);
 }
--- pipex.h
+++ pipex.h
@@ -14,10 +14,14 @@
 # define PIPEX_H
 
 /*---------------- HEADERS ----------------*/
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdarg.h>
+# include <unistd.h>
+# include <stdlib.h>
 
 /*----------- FUNCTION + UTILS ------------*/
+int 	check_errors(char **av, char *cmd1, char *cmd2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char *s1, char *s2);
+char	**ft_split(char const *s, char c);
+int     ft_pipex(char **av);
 
 #endif
